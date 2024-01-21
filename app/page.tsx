eb576926b55f9c3e17b1d5a6b47f8d73ee40fb76
parisosuch-dev <parisosuch.dev@gmail.com>
2024-01-20 17:50:54 -0800

    Add skills.

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import About from "@/components/about";
 import Education from "@/components/education";
 import Experience from "@/components/experience";
 import Header from "@/components/header";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Experience />
       <Education />
+      <Skills />
     </div>
   );
 }
---
+++ components/skills.tsx
@@ -0,0 +1,40 @@
+import { Badge } from "./ui/badge";
+
+let skills = [
+  "Python",
+  "C++",
+  "C",
+  "SQL",
+  "Javascript",
+  "Typescript",
+  "NextJS",
+  "React",
+  "NodeJS",
+  "Tailwindcss",
+  "UNIX",
+  "Git",
+  "Drupal",
+  "Firebase",
+  "Supabase",
+  "Docker",
+  "Django",
+  "Flask",
+  "Pandas",
+  "Matplotlib",
+  "Figma",
+];
+
+export default function Skills() {
+  return (
+    <div className="w-full space-y-2">
+      <h1 className="text-2xl font-bold">Skills</h1>
+      <div className="space-x-1">
+        {skills.map((skill, index) => (
+          <Badge variant="outline" key={index}>
+            {skill}
+          </Badge>
+        ))}
+      </div>
+    </div>
+  );
+}
