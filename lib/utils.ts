d4976454220131aa5eea4d7a9e26b2a068ede4b9
Paris Osuch <parisosuch.dev@gmail.com>
2024-02-23 14:53:05 -0800

    Create utility to get previous dates

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { format, subDays } from "date-fns"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getDate(sub: number = 0) {
+  const dateXDaysAgo = subDays(new Date(), sub);
+
+  return format(dateXDaysAgo, "dd/MM/yyyy")
 }
