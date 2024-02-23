4ee31372e85762f2b733954a079d52ef607877ba
Paris Osuch <parisosuch.dev@gmail.com>
2024-02-23 14:53:25 -0800

    Set date up in db for page views

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,11 +1,16 @@
 import { redis } from "./redis"
+import { getDate } from "./utils"
 
 type AnalyticsArgs = {
     retention?: number
 }
 
+type TrackOptions = {
+    persist?: boolean
+}
+
 export class Analytics {
-    private retention: number = 60 * 60 * 24 * 7 // week in seconds
+    private retention: number = 60 * 60 * 24 * 7 * 4 // month in seconds
 
     constructor(opts?: AnalyticsArgs) {
         if (opts?.retention) {
@@ -14,12 +19,22 @@ export class Analytics {
     }
 
     // track an event
-    async track(namespace: string, event: object = {}) {
+    async track(namespace: string, event: object = {}, opts?: TrackOptions) {
         // create table key
-        const key = `analytics::${namespace}`
+        let key = `analytics::${namespace}`
+
+        // if there is no option to persist, expire the data and use a date
+        if (!opts?.persist) {
+            key += `::${getDate()}`
+        }
 
         // db call to persist the event
-        await redis.hincrby(key, JSON.stringify(event), 1);
+        await redis.hincrby(key, JSON.stringify(event), 1)
+
+        // if there is no persistence, expire the key after the retention period
+        if (!opts?.persist) {
+            await redis.expire(key, this.retention)
+        }
     }
 }
 
