1ce574fe075be9df3c56cee4fe3aef37cc48eba1
parisosuch-dev <parisosuch.dev@gmail.com>
2024-02-24 18:33:37 -0800

    fix ?

--- middleware.ts
+++ middleware.ts
@@ -26,5 +26,5 @@ export default async function middlware(req: NextRequest) {
 
 // determine what paths middleware runs on
 export const config = {
-  matcher: ["'/((?!api|_next/static|_next/image|favicon.ico|analytics).*)'"],
+  matcher: "'/((?!api|analytics|_next/static|_next/image|favicon.ico).*)'",
 };
