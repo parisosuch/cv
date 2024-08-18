e672800684a9eb536b11b7fc95a58c99bca16ba0
paris osuch <paris.osuch@gmail.com>
2024-08-17 22:45:30 -0700

    Wrap layout with provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 
 import "./globals.css";
+import { CSPostHogProvider } from "./providers";
 
 export const metadata: Metadata = {
   title: "Paris Osuch",
@@ -17,12 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <CSPostHogProvider>
       <body className={`${GeistSans.variable} ${GeistMono.variable}`}>
         <div className="w-full flex justify-center p-2 sm:p-0 bg-gray-100">
           {children}
           <Analytics />
         </div>
       </body>
+      </CSPostHogProvider>
     </html>
   );
 }
