ea9ba8d6e3ad295197a0ae0be3e1c212aac88de2
paris osuch <paris.osuch@gmail.com>
2025-01-03 21:24:17 -0800

    test might need to revert

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        REDIS_KEY: process.env.REDIS_KEY,
-    }
+  env: {
+    REDIS_KEY: process.env.REDIS_KEY,
+  },
+  rewrites() {
+    return {
+      beforeFiles: [
+        // if the host is `app.acme.com`,
+        // this rewrite will be applied
+        {
+          source: "/:path*",
+          has: [
+            {
+              type: "host",
+              value: "app.acme.com",
+            },
+          ],
+          destination: "/app/:path*",
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
