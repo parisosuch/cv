b9d77199183aed96df3fc234ea68102d56f2d163
paris osuch <paris.osuch@gmail.com>
2025-01-03 21:29:01 -0800

    remove rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -3,24 +3,6 @@ const nextConfig = {
   env: {
     REDIS_KEY: process.env.REDIS_KEY,
   },
-  rewrites() {
-    return {
-      beforeFiles: [
-        // if the host is `app.acme.com`,
-        // this rewrite will be applied
-        {
-          source: "/:path*",
-          has: [
-            {
-              type: "host",
-              value: "app.parisosuch.com",
-            },
-          ],
-          destination: "/app/:path*",
-        },
-      ],
-    };
-  },
 };
 
 export default nextConfig;
