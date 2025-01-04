48e0d877e7dd5cb48c2e7fac7f84e01b20b3a8a9
parisosuch-dev <parisosuch.dev@gmail.com>
2025-01-04 11:07:19 -0800

    remove upstash

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,9 @@
 export default function Footer() {
-    let year = new Date().getFullYear()
+  let year = new Date().getFullYear();
 
-    return (
-        <div className="w-full text-center">
-            <p className="text-xs">designed by paris osuch // {year}</p>
-            <a href="/analytics" className="underline text-xs">site analytics</a>
-        </div>
-    )
+  return (
+    <div className="w-full text-center">
+      <p className="text-xs">designed by paris osuch // {year}</p>
+    </div>
+  );
 }
--- lib/redis.ts
+++
@@ -1,6 +0,0 @@
-import { Redis } from '@upstash/redis'
-
-export const redis = new Redis({
-    url: 'https://usw1-unique-martin-33947.upstash.io',
-    token: process.env.REDIS_KEY!,
-})
