f1f6991b6d6aa47874b69204eadf1a218837fe1b
Paris Osuch <paris.osuch@gmail.com>
2025-08-10 19:39:47 -0700

    update experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -8,24 +8,21 @@ let experience = [
   {
     name: "Nelson & Kennard - Sacramento, CA",
     position: "Junior Software Engineer",
-    summary:
-      "Automated business processes using Python, Pandas, and SQL. Developed scalable back-end services with Django and Flask, designed user-friendly interfaces using React and Tailwind CSS. Created REST API Logging microservice, applied Pandas for data analysis, enhancing efficiency and supporting data-driven decisions.",
+    summary: "Automated business processes using Python and T-SQL. Managed a legacy automation engine built in Flask. Led and coordinated a project to migrate the legacy automation engine to a Django RESTful API. This project included a front-end application built in NextJS 13 and Typescript.",
     start: "2023",
     end: "2024",
   },
   {
     name: "Sonoma State University - Rohnert Park, CA",
     position: "Undergraduate Researcher",
-    summary:
-      "Recipient of the Koret Scholarship to develop the open-source Python library QCpy, focusing on simulating quantum circuits. Part of an undergraduate team uniquely invited to SIGCSE 2023, a prestigious international conference for computer science education. Coauthored a peer-reviewed abstract published in the ACM Digital Library of research papers.",
-    start: "2021",
+    summary: "Recipient of the Koret Scholarship to develop an open-source Python library to simulate quantum computation, called QCpy. This research was part of an undergraduate team uniquely invited to SIGCSE 2023, a renowned international conference for computer science education. Research included the coauthoring of a peer-reviewed abstract published in the ACM Digital Library of research papers.  ", start: "2021",
     end: "2023",
   },
   {
     name: "Sonoma State University - Rohnert Park, CA",
     position: "Web Developer",
     summary:
-      "Utilized Drupal, HTML5, CSS3, and JavaScript to generate content for department sites. Achieved a 12% increase in site traffic, as per Google Analytics, through effective Search Engine Optimization (SEO) strategies. Automated legacy site content retrieval using web scraping with Beautiful Soup 4 in Python.",
+      "Utilized Drupal, HTML5, CSS3, and JavaScript to create and manage content for univeristy department sites. Took part in a campaign to optimize department sites through Search Engine Optimization (SEO) strategies. Was part of a project to automate legacy site content retrieval using web scraping with Beautiful Soup 4 in Python.",
     start: "2019",
     end: "2021",
   },
